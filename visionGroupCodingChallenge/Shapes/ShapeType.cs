290cd54446547919c728a9c0b2772e436da855b2
Chad Quinlan <quinla34@msu.edu>
2022-07-29 13:25:31 -0400

    added enum shape types

--- visionGroupCodingChallenge/Shapes/ShapeType.cs
+++ visionGroupCodingChallenge/Shapes/ShapeType.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace visionGroupCodingChallenge.Shapes
+﻿namespace visionGroupCodingChallenge.Shapes
 {
+    // Enum for the different shapes
     public enum ShapeType
     {
+        Square,
+        Ellipse,
+        Circle,
+        Triangle,
+        Polygon
 
     }
 }
