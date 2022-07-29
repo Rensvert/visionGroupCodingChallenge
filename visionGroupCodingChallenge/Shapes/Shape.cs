5c75f93e1ab621e62c1ec7f82bb51e7eb8962917
Chad Quinlan <quinla34@msu.edu>
2022-07-29 13:22:50 -0400

    Add project files.

---
+++ visionGroupCodingChallenge/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace visionGroupCodingChallenge
+{
+    public class Shape
+    {
+        Shapes.ShapeType type;
+
+        public Shape(Shapes.ShapeType type)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ visionGroupCodingChallenge/Shapes/ShapeType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace visionGroupCodingChallenge.Shapes
+{
+    public enum ShapeType
+    {
+
+    }
+}
